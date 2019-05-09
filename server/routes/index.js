bbc6df489520c0fdba48f2465009b8237b1dc171
suleyunus <suleimanyunus778@gmail.com>
2019-05-09 19:27:19 +0300

    ft(routes): separate routes from server.js to index.js

---
+++ server/routes/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import users from '../models/mock-db';
+
+const router = express.Router();
+
+router.post('/api/v1/signin', (req, res) => {
+  if (req.body.email === users[0].email
+        && req.body.password === users[0].password) {
+    res.json('success');
+  } else {
+    res.status(400).json('error logging in');
+  }
+});
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -1,47 +1,11 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
-import users from './models/mock-db';
+import bodyParser from 'body-parser';
+import router from './routes/index';
 
 const app = express();
-
-app.get('/api', (req, res) => {
-  res.json({
-    message: 'welcome to the API',
-  });
-});
-
-app.post('/api/posts', verifyToken, (req, res) => {
-  jwt.verify(req.token, 'secretkey', (err, authData) => {
-    if (err) {
-      res.sendStatus(403);
-    } else {
-      res.json({
-        message: 'Post Created...',
-        authData,
-      });
-    }
-  });
-});
-
-app.post('/api/login', (req, res) => {
-  jwt.sign({ user: users[0] }, 'secretkey', { expiresIn: '60s'}, (err, token) => {
-    res.json({ token });
-  });
-});
-
-function verifyToken(req, res, next) {
-  const bearerHeader = req.headers.authorization;
-
-  if (typeof bearerHeader !== 'undefined') {
-    const bearer = bearerHeader.split(' ');
-    const bearerToken = bearer[1];
-    req.token = bearerToken;
-    next();
-
-  } else {
-    res.sendStatus(403);
-  }
-}
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(router);
 
 const PORT = process.env.PORT || 3000;
 
