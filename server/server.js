05a9c4fb0dfc240aeced60e62c5746f4fbee3355
suleyunus <suleimanyunus778@gmail.com>
2019-05-10 10:25:26 +0300

    ft(versioning): add api url versioning

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(router);
-app.use('/loans', loanRoutes);
+app.use('api/v1/loans', loanRoutes);
 
 const PORT = process.env.PORT || 3000;
 
