3bdeec9dd6dd78085860c0af77c3a013b09525f5
suleyunus <suleimanyunus778@gmail.com>
2019-05-10 14:26:55 +0300

    ch(log-apis): add morgan to log apis from postman [starts #165934332]

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import morgan from 'morgan';
 import router from './routes/users';
 import loanRoutes from './routes/loans';
 
 const app = express();
+
+app.use(morgan('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Routes which should handle requests
 app.use(router);
 app.use('/api/v1/loans', loanRoutes);
 
