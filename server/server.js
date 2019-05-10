5f3dd5382132df58f99d364b2b74ed4228175995
suleyunus <suleimanyunus778@gmail.com>
2019-05-10 14:40:28 +0300

    ch(error-handling): add error message to handle non-defined routes (commented) [finishes #165934332]

--- server/server.js
+++ server/server.js
@@ -15,6 +15,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(router);
 app.use('/api/v1/loans', loanRoutes);
 
+// app.use((req, res, next) => {
+//   const error = new Error('Not Found');
+//   error.status = 404;
+//   next(error);
+// })
+
+// app.use((error, req, res, next) => {
+//   res.status(error.status || 500);
+//   res.json({
+//     error: {
+//       message: error.message
+//     }
+//   })
+// });
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
