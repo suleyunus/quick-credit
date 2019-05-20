3991135ef5c6e6c24dea2345ea6fc73ebdf37dde
suleyunus <suleimanyunus778@gmail.com>
2019-05-20 11:13:20 +0300

    ft(auth): remove auth control

--- server/routes/loans.js
+++ server/routes/loans.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import authControl from '../middlewares/authcontroller';
 import loansControllers from '../controllers/loans';
 
 const router = express.Router();
@@ -8,7 +7,7 @@ router.get('/', loansControllers.getLoans);
 
 router.get('/:loanID', loansControllers.getByID);
 
-router.post('/', authControl, loansControllers.createLoan);
+router.post('/', loansControllers.createLoan);
 
 router.patch('/:loanID', loansControllers.updateLoanStatus);
 
