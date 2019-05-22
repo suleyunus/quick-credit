88c61593c3432c4342bc0414c248065914a264cd
suleyunus <suleimanyunus778@gmail.com>
2019-05-23 02:07:04 +0300

    ft(loans-tables): add create loans tables to db.js

--- server/db.js
+++ server/db.js
@@ -15,7 +15,7 @@ const createUsersTables = () => {
   const queryText = `CREATE TABLE IF NOT EXISTS
   users(
     id serial PRIMARY KEY,
-    email VARCHAR(50) NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
     firstName VARCHAR(15) NOT NULL,
     lastName VARCHAR(15),
     password VARCHAR(50),
@@ -36,7 +36,47 @@ const createUsersTables = () => {
     });
 };
 
+const createLoansTables = () => {
+  const queryText = `CREATE TABLE IF NOT EXISTS
+  loans(
+    id serial PRIMARY KEY,
+    email VARCHAR(50) NOT NULL,
+    createdOn TIMESTAMP NOT NULL,
+    status VARCHAR(10) NOT NULL,
+    repaid VARCHAR(10) NOT NULL,
+    tenor INT NOT NULL,
+    amount FLOAT(2) NOT NULL,
+    paymentInstallment FLOAT(2) NOT NULL,
+    balance FLOAT(2) NOT NULL,
+    interest FLOAT(2) NOT NULL,
+    FOREIGN KEY (email) references users (email) ON DELETE CASCADE
+  )`;
+
+  pool.query(queryText)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+};
+
 const dropUsersTables = () => {
+  const queryText = 'DROP TABLE IF EXISTS users';
+  pool.query(queryText)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+};
+
+const dropLoansTables = () => {
   const queryText = 'DROP TABLE IF EXISTS loans';
   pool.query(queryText)
     .then((res) => {
@@ -49,6 +89,16 @@ const dropUsersTables = () => {
     });
 };
 
+const createAllTables = () => {
+  createUsersTables();
+  createLoansTables();
+};
+
+const dropAllTables = () => {
+  dropUsersTables();
+  dropLoansTables();
+};
+
 pool.on('remove', () => {
   console.log('client removed');
   process.exit(0);
@@ -57,6 +107,10 @@ pool.on('remove', () => {
 module.exports = {
   createUsersTables,
   dropUsersTables,
+  createLoansTables,
+  dropLoansTables,
+  createAllTables,
+  dropAllTables,
 };
 
 require('make-runnable');
