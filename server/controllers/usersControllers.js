d750461dbfa33de5ae03582db6f3e4e747171a24
suleyunus <suleimanyunus778@gmail.com>
2019-05-23 04:10:42 +0300

    ch(auth): add auth and tokens [finishes #166200027]

--- server/controllers/usersControllers.js
+++ server/controllers/usersControllers.js
@@ -1,8 +1,11 @@
 import moment from 'moment';
 import db from '../db/index';
+import AuthHelper from '../helpers/auth';
 
 class Users {
   static async signup(req, res) {
+    const hashPassword = AuthHelper.hashPassword(req.body.password);
+
     const text = `INSERT INTO
       users(email, firstName, lastName, password, address, status, isAdmin, dateJoined)
       VALUES($1, $2, $3, $4, $5, $6, $7, $8)
@@ -11,38 +14,55 @@ class Users {
       req.body.email,
       req.body.firstName,
       req.body.lastName,
-      req.body.password,
+      hashPassword,
       req.body.address,
       'unverified',
-      false,
+      req.body.isAdmin,
       moment(new Date()),
     ];
 
     try {
       const { rows } = await db.query(text, values);
-      return res.status(201).send(rows[0]);
+      const token = AuthHelper.generateToken(rows[0].email);
+      return res.status(201).json({
+        status: 201,
+        data: token,
+      });
     } catch (error) {
+      if (error.routine === '_bt_check_unique') {
+        return res.status(400).json({
+          status: 400,
+          message: 'User with that Email already exists',
+        });
+      }
       return res.status(400).send(error);
     }
   }
 
   static async signin(req, res) {
     const text = 'SELECT * FROM users WHERE email = $1';
-    const { email } = req.body;
 
     try {
-      const { rows } = await db.query(text, [email]);
+      const { rows } = await db.query(text, [req.body.email]);
       if (!rows[0]) {
         return res.status(400).send({
           message: 'No account found',
         });
       }
-      return res.status(200).send({
+      if (!AuthHelper.comparePassword(rows[0].password, req.body.password)) {
+        return res.status(401).json({
+          status: 401,
+          message: 'Unauthorized',
+        });
+      }
+
+      const token = AuthHelper.generateToken(rows[0].email);
+      return res.status(200).json({
         status: 200,
-        data: rows[0],
+        data: token,
       });
     } catch (error) {
-      res.status(400).send(error)
+      res.status(400).send(error);
     }
   }
 }
